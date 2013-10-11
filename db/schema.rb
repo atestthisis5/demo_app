c5466259e4f416f747186148d2095df2889170da
atestthisis5 <atestthisis5@gmail.com>
2013-10-11 00:37:11 -0400

    migrate db with Micropost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131011043143) do
+ActiveRecord::Schema.define(:version => 20131011043620) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "contemt"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
